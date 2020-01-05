462553125e4c4266380130f83c33fa08c9843600
orson.s.h.zhu <orson.s.h.zhu@outlook.com>
2020-01-05 10:54:03 +0800

    init: project

---
+++ docker-file-simple/src/main/java/com/orson/docker/DockerFileSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.orson.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerFileSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerFileSimpleApplication.class, args);
+	}
+
+}
---
+++ docker-file-simple/src/test/java/com/orson/docker/DockerFileSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orson.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerFileSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
