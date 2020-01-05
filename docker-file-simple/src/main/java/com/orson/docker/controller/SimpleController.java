4b8e238ed2b4598a94cc2cf06f6cd012961e7b1a
orson.s.h.zhu <orson.s.h.zhu@outlook.com>
2020-01-05 11:16:16 +0800

    docker: add dockerfile

---
+++ docker-file-simple/src/main/java/com/orson/docker/controller/SimpleController.java
@@ -0,0 +1,16 @@
+package com.orson.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class SimpleController {
+	
+	
+	@GetMapping("/simple")
+	public String simple() {
+		return "Hello, this is from docker-file-simple";
+	}
+}
