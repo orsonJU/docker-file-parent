36ff3bf6fc8ef985257aca30a79547ab06cd5edf
orson.s.h.zhu <orson.s.h.zhu@outlook.com>
2020-01-05 11:42:00 +0800

    new module: image-from-container

---
+++ image-from-container/src/main/java/com/orson/docker/ImageFromContainerApplication.java
@@ -0,0 +1,13 @@
+package com.orson.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageFromContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageFromContainerApplication.class, args);
+	}
+
+}
---
+++ image-from-container/src/test/java/com/orson/docker/ImageFromContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orson.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageFromContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
